7549e98495c001bd8645a2c81955bba4dafd3c1f
HarshUleedri <ulidari2@gmail.com>
2025-07-01 16:12:49 +0530

    fix: :bug: unused route removed from protected routes

--- src/routes/ProtectedRoutes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes/ProtectedRoutes.tsx
@@ -1,17 +1,10 @@
 import ProtectedLayout from "@/Layout/ProtectedLayout";
 import Consultation from "@/pages/Consultation/Consultation";
-import SingleConsultationData from "@/pages/Dashboard/PatientDashboard/components/SingleConsultationData";
 
 export const ProtectedRoutes = [
   {
     path: "/dashboard",
     element: <ProtectedLayout />,
-    children: [
-      {
-        path: "consultation/:id",
-        element: <SingleConsultationData />,
-      },
-    ],
   },
   {
     path: "/consult/:id",
